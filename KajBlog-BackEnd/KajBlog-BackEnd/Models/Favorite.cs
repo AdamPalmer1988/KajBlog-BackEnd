625a6e8ffdba03bf71e6e351188c1e7f00f0ce88
Adam Palmer <adampalmer@rocketmortgage.com>
2024-09-30 21:45:31 -0400

    Git Ignore

---
+++ KajBlog-BackEnd/KajBlog-BackEnd/Models/Favorite.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KajBlog_BackEnd.Models;
+
+public class Favorite
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public int UserId { get; set; }
+
+    [Required]
+    public int BlogId { get; set; }
+
+}
