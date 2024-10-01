8517318a049711c73384eced852a56ea4153659d
Jasmine Boykins <jasmine_boykins@yahoo.com>
2024-09-30 21:39:22 -0400

    Blog Model

---
+++ KajBlog-BackEnd/KajBlog-BackEnd/Models/Blog.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KajBlog_BackEnd.Models
+{
+    public class Blog
+    {
+        [Key]
+        public int BlogID { get; set; }
+        public string UserID { get; set; } 
+        public string Category { get; set; }
+        public string SubjectLine { get; set; }
+        public string BlogBody { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string GiphyPull { get; set; } 
+
+
+    }
+}
