43deb1db9229931ca88c2b4958f0bd84a59224f2
Adam Palmer <adampalmer@rocketmortgage.com>
2024-09-30 21:15:25 -0400

    UserModel Push

---
+++ KajBlog-BackEnd/KajBlog-BackEnd/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KajBlog_BackEnd.Models;
+
+public class User
+{
+    [Key]
+    public int UserId { get; set; }
+
+    [Required]
+    public string UserName { get; set; }
+}
