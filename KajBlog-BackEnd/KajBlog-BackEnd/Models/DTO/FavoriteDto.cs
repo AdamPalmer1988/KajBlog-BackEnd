bc91aacd219920f0e428b36ff9ccd495bf4097da
Jasmine Boykins <jasmine_boykins@yahoo.com>
2024-10-05 18:47:52 -0400

    Added the Favorite Dto

---
+++ KajBlog-BackEnd/KajBlog-BackEnd/Models/DTO/FavoriteDto.cs
@@ -0,0 +1,12 @@
+﻿
+namespace KajBlog_BackEnd.Models.DTO;
+
+public class FavoriteDto
+{
+    public int Id { get; set; }
+
+    public int UserId { get; set; }
+
+    public int BlogId { get; set; }
+
+}
---
+++ KajBlog-BackEnd/KajBlog-BackEnd/Models/DTO/UpdateFavoriteDtoClass.cs
@@ -0,0 +1,10 @@
+﻿namespace KajBlog_BackEnd.Models.DTO;
+
+public class UpdateFavoriteDtoClass
+{
+    public int Id { get; set; }
+
+    public int UserId { get; set; }
+
+    public int BlogId { get; set; }
+}
